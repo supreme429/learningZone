8f6b34f1b83a27472a05c4d61b8b0e903a4f3e80
liuyaowen <liuyaowen@meilele.com>
2019-08-13 13:34:14 +0800

    some modefine of test

--- vue-typescript/src/views/About.vue
+++ vue-typescript/src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="clickThis">clickME</button>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  data() {
+    return {
+      testLog: 'dfad',
+    };
+  },
+  methods: {
+    clickThis(): void {
+      console.log(this.testLog);
+    },
+  },
+});
+</script>
